47541efcde547f692b22729b07650cae6b8b44da
axelstram <astraminsky@dc.uba.ar>
2016-10-30 00:19:29 -0300

    mas del punto 1

--- trace.py
+++ trace.py
@@ -40,26 +40,50 @@ class Hop:
 
 
 
+def HuboRespuesta(hop):
+	if hop.ip not 0:
+		return true
+	else
+		return false
 
-def calcularRuta(host, ttl):
+
+def CalcularRuta(host, ttl):
 	ruta = []
 
 	for ttl in range(1, 50):
 		hop = Hop()
 
-		for rafaga in range(1, 10):
-			ans, unans = sr(IP(dst=host, ttl=ttl) / ICMP(), timeout=10)
+		for rafaga in range(1, 3):
+			packet = IP(dst=host, ttl=ttl) / ICMP()
+			ans, unans = sr(packet, timeout=10)
 
-			if len(ans) != 0:
+			if len(ans) != 0: #hubo respuesta
 				packet_sent = ans[0][0]
 				answer = ans[0][1]
 
-				if (answer.type == PacketType.TimeExceeded):
+				if (answer.type == PacketType.EchoReply): #Llegue al host
 					rtt = (answer.time - packet_sent.sent_time)
+					hop.ip = answer.src
 					hop.rttlist.append(rtt)
+					break
 
+				if (answer.type == PacketType.TimeExceeded): #No llegue. Saco los datos host que me devolvio este paquete ICMP
+					rtt = (answer.time - packet_sent.sent_time)
+					hop.ip = answer.src 	#SERIOUS SHIT: PREGUNTAR QUE PASA SI TENGO VARIAS RUTAS DISTINTAS. ¿CON CUAL ME QUEDO?
+					hop.rttlist.append(rtt)
+		#end for
 
+		if HuboRespuesta(hop):
+			hop.rttprom = sum(hop.rttlist)/len(hop.rttlist)
+			#.... aca hay que meterle al hop toda la gilada de geolocalizacion. Por ahi habria que llamar a esas paginas de geolocalizacion
+			#desde el codigo y parsear lo que devuelven, para que quede todo automatico.
+			#despues le agregamos esos campos a la clase hop y lo guardamos ahi.
+			ruta.append(hop)
 
+			if hop.ip == host:
+				break
+
+	#end for
 	return ruta
 
 
